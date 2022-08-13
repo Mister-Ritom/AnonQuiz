9b5b57105b94ac83f82e66612c0e021846da3ba4
Ritom Ghosh <ritomghosh856@gmail.com>
2022-08-13 20:23:48 +0530

    a lot of better code

--- app/src/main/java/com/anons/anonquiz/AnswerActivity.kt
+++ app/src/main/java/com/anons/anonquiz/AnswerActivity.kt
@@ -1,72 +1,51 @@
 package com.anons.anonquiz
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.get
-import com.google.gson.JsonArray
 import com.google.gson.JsonElement
-import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import java.io.InputStream
 import java.io.InputStreamReader
 import java.net.URL
 import java.net.URLConnection
-import kotlin.collections.ArrayList
 
 
 class AnswerActivity : AppCompatActivity() {
+    private var pointsInt = 0
+    private var answered = false
+    private val answers:ArrayList<String> = ArrayList()
+    private var currentPos:Int = 0
+    private var question = ""
+    private var rightAnswer = ""
+    private lateinit var root: JsonElement
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_answer)
         val questionText = findViewById<TextView>(R.id.question_txt)
         val answersList = findViewById<ListView>(R.id.answers_list)
         val nextButton = findViewById<Button>(R.id.next_button)
+        val pointsText = findViewById<TextView>(R.id.points)
         val url = URL(intent.getStringExtra("url"))
-        var question = ""
-        val answers:MutableList<String> = ArrayList()
-        var rightAnswer = ""
-        var rootobj:JsonObject? = null
-        var results: JsonArray? = null
-        var position = 0
-        var currentObject: JsonObject?
-        var answered = false
+        val adapter: ArrayAdapter<String> =
+            ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, answers)
         val thread = Thread {
             try {
-                if (rootobj == null && results == null) {
-                    val request: URLConnection = url.openConnection()
-                    request.connect()
-                    val root: JsonElement =
-                        JsonParser.parseReader(InputStreamReader(request.content as InputStream)) //Convert the input stream to a json element
-                    rootobj = root.asJsonObject
-                    results=rootobj!!.getAsJsonArray("results")
-                    currentObject = results!!.get(position).asJsonObject
-                    question = currentObject!!.getAsJsonPrimitive("question").asString
-                    rightAnswer = currentObject!!.getAsJsonPrimitive("correct_answer").asString
-                    answers.add(rightAnswer)
-                    for (je in currentObject!!.getAsJsonArray("incorrect_answers")) {
-                        answers.add(je.asJsonPrimitive.asString)
-                    }
-                }
+                val request: URLConnection = url.openConnection()
+                request.connect()
+                root = JsonParser.parseReader(InputStreamReader(request.content as InputStream))
+                getAnswers()
             } catch (e: Exception) {
                 e.printStackTrace()
-            }
-            finally {
+            } finally {
                 runOnUiThread {
                     questionText.text = question
-                    answers.shuffle()
-                    val adapter:ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,answers)
                     answersList.adapter = adapter
-                    answersList.setOnItemClickListener { parent, view, position, id ->
-                        if (rightAnswer.equals(answers.get(position),true)) {
-                            answersList[position].setBackgroundColor(getColor(R.color.colorAccent))
-                        }
-                        else answersList.setBackgroundColor(getColor(R.color.red))
-                        answered = true
+                    answersList.setOnItemClickListener { _, _, position, _ ->
+                        onClickList(answersList,position,answers.indexOf(rightAnswer))
                     }
                 }
             }
@@ -75,31 +54,49 @@ class AnswerActivity : AppCompatActivity() {
         thread.start()
 
         nextButton.setOnClickListener {
-            position++
+            currentPos++
             if (answered) {
-                answers.removeAll(answers)
-                currentObject = results!!.get(position).asJsonObject
-                question = currentObject!!.getAsJsonPrimitive("question").asString
-                rightAnswer = currentObject!!.getAsJsonPrimitive("correct_answer").asString
-                answers.add(rightAnswer)
-                for (je in currentObject!!.getAsJsonArray("incorrect_answers")) {
-                    answers.add(je.asJsonPrimitive.asString)
-                }
+                answered = false
+                getAnswers()
                 questionText.text = question
-                answers.shuffle()
-                val adapter:ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,answers)
                 answersList.adapter = adapter
-                answersList.setOnItemClickListener { parent, view, position, id ->
-                    if (rightAnswer.equals(answers.get(position),true)) {
-                        answersList[position].setBackgroundColor(getColor(R.color.colorAccent))
-                    }
-                    else answersList.setBackgroundColor(getColor(R.color.red))
-                    answered = true
-                }
+                pointsText.text = getString(R.string.points)+pointsInt
             }
-            Log.w("Next Button","Trying to skip the level")
+            Log.w("Next Button", "Trying to skip the level")
         }
 
+    }
+
+    private fun getAnswers() {
+        answers.clear()
+        val rootobj = root.asJsonObject
+        val results = rootobj.getAsJsonArray("results")
+        val currentObject = results.get(currentPos).asJsonObject
+        question = currentObject!!.getAsJsonPrimitive("question").asString
+        rightAnswer = currentObject.getAsJsonPrimitive("correct_answer").asString
+        answers.add(rightAnswer)
+        for (je in currentObject.getAsJsonArray("incorrect_answers")) {
+            answers.add(je.asJsonPrimitive.asString)
+        }
+        answers.shuffle()
+    }
+
+    private fun onClickList(list:ListView, position:Int, right:Int) {
+        if (right==position) {
+            pointsInt++
+            setPoints(pointsInt)
+        }
+        list.setBackgroundColor(getColor(R.color.white))
+        list[right].setBackgroundColor(getColor(R.color.instacenterclord))
+        answered = true
+    }
+
+    private fun setPoints(points:Int) {
+        PreferenceManager.getDefaultSharedPreferences(this).edit().putInt("points",points).apply()
+    }
+
+    fun getPoints():Int {
+        return PreferenceManager.getDefaultSharedPreferences(this).getInt("points",0)
     }
 
 }
